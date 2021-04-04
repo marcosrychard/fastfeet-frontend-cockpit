18e007f7871c224e1a12680df065776f675ced2e
marcos.rychard <marcos_rychard@hotmail.com>
2021-04-03 21:10:22 -0300

    update angular

--- src/app/modules/deliveryman/store/selectors/delivery.selectors.ts
+++
@@ -1,8 +0,0 @@
-import { createFeatureSelector, createSelector } from '@ngrx/store';
-import { IState, FEATURE_NAME } from '../states/deliveryman.state';
-
-const FIND_STATE = createFeatureSelector<IState>(FEATURE_NAME);
-
-export const  FIND_IS_LOADING = createSelector(FIND_STATE, state => state.loading);
-
-export const FIND_DELIVERYMAN = createSelector(FIND_STATE, state => state.payload);
